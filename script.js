1a28072588c9b1b2a48747db5e7ea21de8641fef
Tobias Nölle <tobias.noelle@icloud.com>
2023-12-09 14:09:23 +0100

    add a svg animation for circle and cross as a function

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ let fields = [
     null
   ];
 
-  render();
+  function init() {
+    render();
+  }
+  
 
   function render() {
     const content = document.getElementById('content');
@@ -25,9 +28,9 @@ let fields = [
   
         // Check the value in the fields array and set the appropriate symbol
         if (fields[index] === 'circle') {
-          tableHTML += 'O';
+          tableHTML += generateAnimatedCircle();
         } else if (fields[index] === 'cross') {
-          tableHTML += 'X';
+          tableHTML += generateAnimatedCross();
         }
   
         tableHTML += '</td>';
@@ -40,3 +43,34 @@ let fields = [
     content.innerHTML = tableHTML;
   }
   
+
+  function generateAnimatedCircle() {
+    const svgCode = `
+      <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="35" cy="35" r="30" fill="none" stroke="#00B0EF" stroke-width="5" 
+          stroke-dasharray="188.5" stroke-dashoffset="188.5">
+          <animate attributeName="stroke-dashoffset" from="188.5" to="0" dur="0.5s" begin="0s" fill="freeze" />
+        </circle>
+      </svg>
+    `;
+  
+    return svgCode;
+  }
+
+
+  function generateAnimatedCross() {
+    const svgCode = `
+      <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg">
+        <line id="line1" x1="10" y1="10" x2="10" y2="10" stroke="#FFC000" stroke-width="5">
+          <animate attributeName="x2" from="10" to="60" dur="0.25s" begin="0s" repeatCount="1" fill="freeze" />
+          <animate attributeName="y2" from="10" to="60" dur="0.25s" begin="0s" repeatCount="1" fill="freeze" />
+        </line>
+        <line id="line2" x1="60" y1="10" x2="60" y2="10" stroke="#FFC000" stroke-width="5">
+          <animate attributeName="x2" from="60" to="10" dur="0.25s" begin="0.25s" repeatCount="1" fill="freeze" />
+          <animate attributeName="y2" from="10" to="60" dur="0.25s" begin="0.25s" repeatCount="1" fill="freeze" />
+        </line>
+      </svg>
+    `;
+  
+    return svgCode;
+  }
